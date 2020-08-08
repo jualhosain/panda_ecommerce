dc6932d24e039380fdd4b0765a3f0399d0bc8ca3
Md Jual Hosain <jualhosain@gmail.com>
2020-08-09 02:19:34 +0600

    new add about,contact, product pages and make them responsive with flex

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+// let select = document.getElementsByClassName('.place-card-large');
+
+// setInterval(function(){
+//     if(select == null){
+//         console.log('work');
+//         console.log(select);
+        
+   
+//     }else{
+//     console.log('1');
+//     console.log(select);
+    
+//     }
+// },1000);
+
